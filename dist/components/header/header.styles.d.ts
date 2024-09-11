b0778ca7a89faa2aa81c5b081c8352ad84865989
jared <jaredkjar@gmail.com>
2024-09-11 15:00:28 -0600

    added build

---
+++ dist/components/header/header.styles.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+export declare const Header: import("styled-components/dist/types").IStyledComponentBase<"web", import("styled-components").FastOmit<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, never>> & string;
+export declare const MediaCloudLogo: import("styled-components/dist/types").IStyledComponentBase<"web", import("styled-components").FastOmit<import("react").DetailedHTMLProps<import("react").ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>, never>> & string;
+export declare const AccountButtonContainer: import("styled-components/dist/types").IStyledComponentBase<"web", import("styled-components").FastOmit<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, never>> & string;
---
+++ dist/components/menu/menu.d.ts
@@ -0,0 +1,1 @@
+export default function Menu(): import("react/jsx-runtime").JSX.Element;
