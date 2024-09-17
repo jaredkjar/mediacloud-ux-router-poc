521e9f6ac807b7b85e9067077669855a3d6e1ccd
jared <jaredkjar@gmail.com>
2024-09-17 14:46:44 -0600

    fix base

--- spa.js
+++ spa.js
@@ -1,1 +1,1 @@
-import{b as a,m as p,u as s}from"./assets/spa-DqaMpHY1.js";import"react";import"react-dom";export{a as bootstrap,p as mount,s as unmount};
+import{b as a,m as p,u as s}from"./assets/spa-CzZFsjEe.js";import"react";import"react-dom";export{a as bootstrap,p as mount,s as unmount};
